f8f4282973526adcbbf7fced06afbed0f70ac60f
Karanveer Singh Khanna <kayvie2@gmail.com>
2018-01-10 19:28:59 -0800

    Game completed

--- RollerBall/Assets/Scripts/PlayerController.cs
+++ RollerBall/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text winText;
+	public Text countText;
 	private Rigidbody rb;
+	private int count;
 
 	void Start() {
-
+		count = 0;
 		rb = GetComponent<Rigidbody> ();
-
+		countText.text = "Count" + count.ToString ();
+		winText.text = "";
 	}
 
 	void FixedUpdate() {
@@ -23,4 +28,21 @@ public class PlayerController : MonoBehaviour {
 
 		rb.AddForce (movement * speed);
     }
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+			count += 1;
+			countText.text = "Count" + count.ToString ();
+		}
+
+		if (count >= 13) {
+			winText.text = "You Win..!!";
+		}
+			
+	} 
+
+
 }
+
+	
