93b506dc4927b153f638b1e385398a15553802c1
Karanveer Singh Khanna <kayvie2@gmail.com>
2018-01-17 22:34:58 -0800

    Bugs need to be fixed for falling platform.

---
+++ ZigZagUnity/Assets/Scripts/BallController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed;
+	private Rigidbody rigidBodyOfBall;
+	private bool start;
+	private bool gameOver;
+
+	void Awake () {
+		rigidBodyOfBall = GetComponent<Rigidbody>();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		start = false;
+		gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!start) {
+			if (Input.GetMouseButtonDown (0)) {
+				rigidBodyOfBall.velocity = new Vector3 (speed, 0, 0);
+				start = true;
+			}
+		}
+
+		if (!Physics.Raycast (transform.position, Vector3.down, 1f)) {
+			gameOver = true;
+			rigidBodyOfBall.velocity = new Vector3(0, -25f, 0);
+			Camera.main.GetComponent<CameraController>().gameOver = true;
+		}
+
+		if (Input.GetMouseButtonDown(0)  && !gameOver) {
+			SwitchDirection();
+		}
+	}
+
+	void SwitchDirection () {
+		if (rigidBodyOfBall.velocity.z > 0) {
+			rigidBodyOfBall.velocity = new Vector3(speed,0,0);
+		} else {
+			rigidBodyOfBall.velocity = new Vector3(0,0,speed);
+		}
+
+	}
+}
---
+++ ZigZagUnity/Assets/Scripts/TriggerChecker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerChecker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerExit (Collider col) {
+		if (col.gameObject.tag == "ball") {
+			Invoke("FallDown", 0.5f);
+		}
+	}
+
+	void FallDown () {
+		GetComponentInParent<Rigidbody>().useGravity = false;
+		GetComponentInParent<Rigidbody>().isKinematic = false;
+		Destroy(transform.parent.gameObject, 2f);
+
+	}
+}
