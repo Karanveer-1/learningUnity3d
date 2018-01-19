58a5600486a244f2defa255ae2a08f46479a5ca2
Karanveer Singh Khanna <kayvie2@gmail.com>
2018-01-18 23:48:46 -0800

    All bugs have been fixed.

--- ZigZagUnity/Assets/Trigger.cs
+++ ZigZagUnity/Assets/Trigger.cs
@@ -22,6 +22,7 @@ public class Trigger : MonoBehaviour {
 
 	void FallDown () {
 		GetComponentInParent<Rigidbody>().useGravity = true;
+		GetComponentInParent<Rigidbody>().isKinematic = false;
 		Destroy(transform.parent.gameObject,2f);
 	}
 }
